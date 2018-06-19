65af4b1f03ab78db3d744b37a1cccbe11ad60c3d
yhlben <yinhengli@sina.com>
2018-06-19 16:44:59 +0800

    增加日志导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,46 +1,46 @@
 module.exports = {
   title: 'yhlben',
   description: '欢迎来到我的空间',
+  ga: 'UA-121061441-1',
   themeConfig: {
     repo: 'yhlben/blog',
     nav: [{
+      text: '日志',
+      link: '/work/'
+    }, {
       text: '关于',
       link: '/about'
     }],
-    sidebar: [
-      {
-        title: '博客',
-        collapsable: false,
-        children: [
-          '/blog/node',
-           '/blog/base-select',
-           '/blog/base-sort',
-           '/blog/docker-demo',
-           '/blog/functional',
-           '/blog/design-pattern',
-           '/blog/graph-http',
-           '/blog/high-algorithm',
-           '/blog/http',
-           '/blog/interview',
-           '/blog/js_inherit',
-           '/blog/js-form',
-           '/blog/linux',
-           '/blog/nginx-conf',
-           '/blog/observer',
-           '/blog/performance',
-           '/blog/redux',
-           '/blog/rxjs',
-           '/blog/senior-sort',
-           '/blog/structure',
-        ]
-      },
-      {
-        title: '生活',
-        children: [ 
-          '/life/2018-06-17'
-        ]
-      }
-    ],
-    lastUpdated: 'Last Updated', 
+    sidebar: {
+      '/blog/': [
+        'node',
+        'base-select',
+        'base-sort',
+        'docker-demo',
+        'functional',
+        'design-pattern',
+        'graph-http',
+        'high-algorithm',
+        'http',
+        'interview',
+        'js_inherit',
+        'js-form',
+        'linux',
+        'nginx-conf',
+        'observer',
+        'performance',
+        'redux',
+        'rxjs',
+        'senior-sort',
+        'structure',
+      ],
+      '/work/': [
+        '2018-06-19',
+      ],
+    },
+    lastUpdated: '最后更新',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '帮助我改善此页面！'
   }
 }
